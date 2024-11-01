dde177c8e284143c536f097c9e1df537ca4fa764
Isac Paulo Pereira <114201145+Isacpp@users.noreply.github.com>
2024-11-01 17:47:08 -0300

    Update EST_CASO_V.sql

--- PORTIFOLIO/DDL/EST_CASO_V.sql
+++ PORTIFOLIO/DDL/EST_CASO_V.sql
@@ -1,1 +1,38 @@
+CREATE SCHEMA 
+
+-- Clientes
+CREATE TABLE Clientes (
+    cliente_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cpf CHAR(11) UNIQUE NOT NULL,
+    data_nascimento DATE,
+    endereco VARCHAR(200),
+    telefone VARCHAR(15),
+    fidelidade ENUM('Sim', 'Não') DEFAULT 'Não'
+);
+
+-- Produtos
+CREATE TABLE Produtos (
+    produto_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    categoria VARCHAR(50),
+    preco DECIMAL(10, 2) NOT NULL,
+    quantidade_estoque INT NOT NULL
+);
+
+-- Fornecedores
+CREATE TABLE Fornecedores (
+    fornecedor_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    contato VARCHAR(100),
+    telefone VARCHAR(15)
+);
+
+-- Pagamentos
+CREATE TABLE Pagamentos (
+    pagamento_id INT PRIMARY KEY AUTO_INCREMENT,
+    valor DECIMAL(10, 2) NOT NULL,
+    data_pagamento DATE NOT NULL,
+    status ENUM('Pago', 'Pendente') NOT NULL
+);
 
